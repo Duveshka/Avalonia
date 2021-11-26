fcf5aa0bc78773abc4faf7b9b7c4d8efee96a554
duveshka <duveshka@mail.ru>
2021-11-26 04:09:37 +0300

    add changes

--- AvaloniaApplication1/AvaloniaApplication1/Buttons.axaml.cs
+++ AvaloniaApplication1/AvaloniaApplication1/Buttons.axaml.cs
@@ -1,13 +1,12 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace AvaloniaApplication1
 {
-    public partial class MainWindow : Window
+    public partial class Buttons : Window
     {
-        public MainWindow()
+        public Buttons()
         {
             InitializeComponent();
 #if DEBUG
@@ -18,14 +17,6 @@ namespace AvaloniaApplication1
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-        private void Button_Click1(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void Button_Click2(object sender, RoutedEventArgs e)
-        {
         }
     }
 }
